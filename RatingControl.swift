b7aa167126089bc77a860ead39e099d1ec60d46e
Andrew Hudson <thaiguy333@yahoo.com>
2016-06-26 17:17:28 -0500

    Finished tutorial down to creating variables for buttonSize. Right before adding Stars. Red buttons come across as too big. Not sure why.

--- RatingControl.swift
+++ RatingControl.swift
@@ -9,26 +9,57 @@
 import UIKit
 
 class RatingControl: UIView {
-
+    
+    // MARK: Properties
+    
+    var rating = 0
+    var ratingButtons = [UIButton]()
+    
+    let spacing = 5
+    
+    let starCount = 5
+    
+   
     // MARK: Initialization
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         
-        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 44, height: 44))
-        button.backgroundColor = UIColor.redColor()
+        for _ in 0..<starCount {
         
-        button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
-        
-        addSubview(button)
-        
-        
+            let button = UIButton()
+            button.backgroundColor = UIColor.redColor()
+            button.addTarget(self, action: #selector(RatingControl.ratingButtonTapped(_:)), forControlEvents: .TouchDown)
+            ratingButtons += [button]
+            addSubview(button)
+        }
         
     }
     
     override func intrinsicContentSize() -> CGSize {
-        return CGSize(width: 240, height: 44)
+        let buttonSize = Int(frame.size.height)
+        let width = (buttonSize * starCount) + (spacing * (starCount - 1))
+        
+        return CGSize(width: width, height: buttonSize)
     }
+
+    
+        override func layoutSubviews() {
+            // Set the button's width and height to a square the size of the frame's height.
+            let buttonSize = Int(frame.size.height)
+            var buttonFrame = CGRect(x: 0, y: 0, width: buttonSize, height: buttonSize)
+            
+            // Offset each button's origin by the length of the button plus spacing.
+            for (index, button) in ratingButtons.enumerate() {
+                buttonFrame.origin.x = CGFloat(index * (buttonSize + spacing))
+                button.frame = buttonFrame
+            }
+        }
+        
+        
+        
+    
+    
     
     // MARK: Button Action
     
